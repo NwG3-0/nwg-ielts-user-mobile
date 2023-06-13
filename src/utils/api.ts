31cb596f508feb299bd4bb31a6ead9cabf8777e4
Tuan Vuong <phamtuanvuong2k@gmail.com>
2023-06-13 15:25:00 +0700

    Add the library

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,9 +1,8 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { USER_INFO } from 'models/api'
-import { DEVICES } from './common'
-import axiosInstance from './axios'
+import axios from 'axios'
 
-export const _API_BASE_URL = 'http://10.31.0.37:4000'
+export const _API_BASE_URL = 'http://192.168.16.170:4000'
 
 export const isLogin = async () => {
   console.log(await AsyncStorage.getItem(USER_INFO))
@@ -11,15 +10,16 @@ export const isLogin = async () => {
 }
 
 export const login = async ({ email, password }: { email: string; password: string }) => {
-    if (email === '') {
-      return { success: false, data: null, message: 'Please enter your email' }
-    }
-    if (password === '') {
-      return { success: false, data: null, message: 'Please enter your password' }
-    }
+  if (email === '') {
+    return { success: false, data: null, message: 'Please enter your email' }
+  }
+  if (password === '') {
+    return { success: false, data: null, message: 'Please enter your password' }
+  }
 
-    const {data} = await axiosInstance.post(`${_API_BASE_URL}/api/auth/login`, {
-     email:email,password:password
-    })
-    return data
+  const { data } = await axios.post(`${_API_BASE_URL}/api/auth/login`, {
+    email: email,
+    password: password,
+  })
+  return data
 }
